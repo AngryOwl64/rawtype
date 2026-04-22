44c3b3a541afda43431241954e5b6c1ec104ce62
laptop owl <l.klemm@hotmail.com>
2026-04-22 15:23:16 +0200

    lang filter

--- sql/setdifficult.sql
+++ sql/setdifficult.sql
@@ -1,7 +1,7 @@
 UPDATE words
-Where language = 'de'
 SET difficulty = CASE
   WHEN LENGTH(word) <= 5 THEN 'easy'::text_difficulty
   WHEN LENGTH(word) <= 10 THEN 'medium'::text_difficulty
   ELSE 'hard'::text_difficulty
-END;
+END
+Where language = 'de';
